5f0c4d7920c4ab2822e5819353786c7e00f93ce3
Mateusz5d <mateusz93w@gmail.com>
2022-03-13 12:19:28 +0100

    Leson.2.2

--- projects/team/src/lib/adapters/primary/ui/our-team.component.ts
+++ projects/team/src/lib/adapters/primary/ui/our-team.component.ts
@@ -1,5 +1,10 @@
 import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
 
-@Component({ selector: 'lib-our-team', templateUrl: './our-team.component.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
+@Component({ 
+     selector: 'lib-our-team',
+     templateUrl: './our-team.component.html',
+     encapsulation: ViewEncapsulation.None,
+     changeDetection: ChangeDetectionStrategy.OnPush })
 export class OurTeamComponent {
+
 }
